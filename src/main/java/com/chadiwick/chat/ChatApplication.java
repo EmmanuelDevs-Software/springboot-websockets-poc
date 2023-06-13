b89f145c31063dca3e58e28575c22638cd19a0cf
Emmanuel Davila <desarrollos.chadwick@gmail.com>
2023-06-13 06:00:57 +0200

    feat: webSockets

---
+++ src/main/java/com/chadiwick/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.chadiwick.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
